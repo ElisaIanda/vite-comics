c70f425ffb33c8e43c3bb7d844d838ceac12ece2
ElisaIanda <elisaianda01@gmail.com>
2023-08-22 20:12:35 +0200

    Aggiunto 2 footer e main

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,40 @@
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
 import Banner from "./components/Banner.vue"
+import ThePiePagina from "./components/ThePiePagina.vue";
 
 export default {
   components: {
   TheHeader,
   TheFooter,
   Banner,
+  ThePiePagina,
 }
 }
 </script>
 
 <template>
   <TheHeader></TheHeader>
-
+<main class="p-3">
+  <div class="container">
+    <h4> Contect Goes Here</h4>
+  </div>
+</main>
   <Banner></Banner>
+
   <TheFooter></TheFooter>
+
+  <ThePiePagina></ThePiePagina>
 </template>
 
-<style >
+<style lang="scss">
+@use"../src/styles/partials/variable" as *;
+main{
+  background-color: black;
+}
 
+h4{
+  color: $my-color-text;
+}
 </style>
 
---
+++ src/components/ThePiePagina.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="bg-piepagina py-4">
+        <div class="container">
+            <div class="d-flex align-items-center justify-content-between">
+                <button>
+                    SIGN-UP NOW!
+                </button>
+
+                <h4>FOLLOW US</h4>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use"../styles/partials/variable" as *;
+
+.bg-piepagina{
+    background-color: $my-color-secondary;
+}
+h4{
+    color: $my-color-primary;
+}
+
+button{
+    border-color: $my-color-primary;
+    background-color: transparent;
+    color: $my-color-text;
+    font-size: 1.5rem;
+}
+</style>
